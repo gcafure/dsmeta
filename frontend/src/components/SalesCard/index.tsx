b885c20b8d5f1f7438ab690c90b5fead4784e300
Guilherme Nascimento Cafure <GNC@bdmg.mg.gov.br>
2022-07-15 16:56:19 -0400

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -17,11 +17,15 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)
+
+        const minDateISOFormated = minDate.toISOString().slice(0, 10);
+        const maxDateISOFormated = maxDate.toISOString().slice(0, 10);
+
+        axios.get(`${BASE_URL}/sales?minDate=${minDateISOFormated}&maxDate=${maxDateISOFormated}`)
             .then(response => {
                 setSales(response.data.content);
             })
-    }, []);
+    }, [minDate, maxDate]);
 
     return (
         <div className="dsmeta-card">
